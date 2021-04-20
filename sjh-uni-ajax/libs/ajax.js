0862f1d471ada163e7c8db6aa737cb8b06b8848c
Mr.su <42265909+Handsome00000@users.noreply.github.com>
2021-04-20 15:08:40 +0800

    Add files via upload

--- sjh-uni-ajax/libs/ajax.js
+++ sjh-uni-ajax/libs/ajax.js
@@ -3,8 +3,8 @@ import ajax from '@/uni_modules/u-ajax/js_sdk'
 
 // 创建请求实例
 const axios = ajax.create({
-	// 默认配置  api 为 h5的反向代理 参考 vue axios 需在 manifest.json 配置  （app，小程序没有跨域）
-	baseURL: '/api', //如不需要解决跨域 直接换成你的http：//*** 地址
+	// 默认配置
+	baseURL: '/api',
 	timeout: 10000
 })
 
@@ -12,12 +12,12 @@ const axios = ajax.create({
 axios.interceptors.request.use(
 	config => {
 		// 在发送请求前做些什么
-		// console.log('发送请求前', config)
+		console.log('发送请求前', config)
 		return config
 	},
 	error => {
 		// 对请求错误做些什么
-		console.log('发请求错误', error)
+		// console.log('发请求错误', error)
 		//网络请求超时
 		uni.showToast({
 			title: "网络超时！",
@@ -31,12 +31,51 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
 	response => {
 		// 对响应数据做点什么
-		console.log('响应成功后', response)
+		// console.log('响应成功后', response.data)
 		return response
 	},
 	error => {
-		// 对响应错误做点什么
+		// 对响应错误做点什么 
 		console.log('响应错误后', error)
+		let code = error.statusCode
+		if (error.data.error) {
+			let errData = error.data.error
+			uni.showToast({
+				title: errData.message,
+				icon: "none"
+			})
+		} else {
+			switch (code) {
+				case 401:
+					// uni.showToast({
+					// 	title: `授权信息已经失效,请重新登录！`,
+					// 	icon: "none",
+					// 	duration: 2500
+					// })
+					uni.showModal({
+						title: '提示',
+						content: '授权信息已经失效,请重新登录！',
+						success: (res) => {
+							if (res.confirm) {
+								console.log("跳转登录")
+								uni.redirectTo({
+									url: "/pages/login/login"
+								})
+							}
+						}
+					});
+					break
+				case 500:
+					uni.showToast({
+						title: `连接失败，请检查网络连接！`,
+						icon: "none",
+						duration: 2500
+					})
+					break
+			}
+
+		}
+
 		return error
 	}
 )
@@ -45,24 +84,26 @@ axios.interceptors.response.use(
 
 export const getRequest = (url, params) => {
 	let Authorization = uni.getStorageSync("access_token");
+	let token_type = uni.getStorageSync("token_type");
 	return axios({
 		method: 'get',
 		url: `${url}`,
 		params: params,
 		header: {
-			'Authorization': Authorization
+			'Authorization': token_type + " " + Authorization
 		}
 	});
 };
 
 export const postRequest = (url, params) => {
 	let Authorization = uni.getStorageSync("access_token");
+	let token_type = uni.getStorageSync("token_type");
 	return axios({
 		method: 'post',
 		url: `${url}`,
 		header: {
 			'Content-Type': 'application/x-www-form-urlencoded', //formdata 格式
-			'Authorization': Authorization
+			'Authorization': token_type + " " + Authorization
 		},
 		data: params,
 	});
@@ -70,38 +111,43 @@ export const postRequest = (url, params) => {
 
 export const postJsonRequest = (url, params) => {
 	let Authorization = uni.getStorageSync("access_token");
+	let token_type = uni.getStorageSync("token_type");
 	return axios({
 		method: 'post',
 		url: `${url}`,
 		data: params,
-		headers: {
+		header: {
 			'Content-Type': 'application/json', //json 格式
-			'Authorization': Authorization
-		}
-	});
-};
-export const deleteRequest = (url, params) => {
-	let Authorization = uni.getStorageSync("access_token");
-	return axios({
-		method: 'delete',
-		url: `${base}${url}`,
-		params: params,
-		headers: {
-			'Content-Type': 'application/json',
-			'Authorization': Authorization
+			'Authorization': token_type + " " + Authorization
 		}
 	});
 };
 
+//params 是 get  data 是 post
 export const deleteJsonRequest = (url, params) => {
 	let Authorization = uni.getStorageSync("access_token");
+	let token_type = uni.getStorageSync("token_type");
 	return axios({
 		method: 'delete',
-		url: `${base}${url}`,
-		data: params,
-		headers: {
+		url: `${url}`,
+		params: params,
+		header: {
 			'Content-Type': 'application/json',
-			'Authorization': Authorization
+			'Authorization': token_type + " " + Authorization
+		}
+	});
+};
+
+export const deleteRequest = (url, params) => {
+	let Authorization = uni.getStorageSync("access_token");
+	let token_type = uni.getStorageSync("token_type");
+	return axios({
+		method: 'delete',
+		url: `${url}`,
+		data: params,
+		header: {
+			'Content-Type': 'application/json',
+			'Authorization': token_type + " " + Authorization
 		}
 	});
 };
